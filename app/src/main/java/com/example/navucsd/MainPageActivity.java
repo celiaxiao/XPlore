62ecdb21eba272d68e1e74c4f8f8456b3d12d7e9
Mingqi Zhang <jameszmq@gmail.com>
2020-03-31 22:45:48 -0700

    Restyled Mainpage UI
    Also made minor change to Landmark Detail page

--- app/src/main/java/com/example/navucsd/MainPageActivity.java
+++ app/src/main/java/com/example/navucsd/MainPageActivity.java
@@ -1,12 +1,16 @@
 package com.example.navucsd;
 
 import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -63,6 +67,27 @@ public class MainPageActivity extends AppCompatActivity {
 		// TODO fix this
 //        recyclerView.setHasFixedSize(true);
 		layoutManager = new LinearLayoutManager(this);
+		DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+				LinearLayoutManager.VERTICAL) {
+			@Override
+			public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+				int position = parent.getChildAdapterPosition(view);
+				// hide the divider for the last child
+				if (position == state.getItemCount() - 1) {
+					outRect.setEmpty();
+				} else {
+					super.getItemOffsets(outRect, view, parent, state);
+				}
+			}
+		};
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			dividerItemDecoration.setDrawable(getDrawable(R.drawable.horizontal_divider_16dp));
+		}
+		else {
+			dividerItemDecoration.setDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.horizontal_divider_16dp));
+		}
+		recyclerView.addItemDecoration(dividerItemDecoration);
+
 		recyclerView.setLayoutManager(layoutManager);
 		mAdapter = new MainPagePlacesAdapter();
 		recyclerView.setAdapter(mAdapter);
