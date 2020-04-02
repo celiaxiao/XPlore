a9fc2f186b2139c94149df8087585d3114315944
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 20:43:27 -0700

    Test run

---
+++ app/src/main/java/com/example/navucsd/Location.java
@@ -0,0 +1,13 @@
+package com.example.navucsd;
+
+import android.util.Pair;
+
+public class Location {
+    public  String name;
+    private String tags;
+    private Pair coordinates;
+    private String description;
+    private Location relatedLocation;
+
+
+}
---
+++ app/src/main/java/com/example/navucsd/LocationFinder.java
@@ -0,0 +1,12 @@
+package com.example.navucsd;
+
+import java.util.ArrayList;
+
+public class LocationFinder {
+    public ArrayList<Location> locations;
+
+    public LocationFinder(){
+        Location geisel = new Location();
+        geisel.name = "Geisel";
+    }
+}
