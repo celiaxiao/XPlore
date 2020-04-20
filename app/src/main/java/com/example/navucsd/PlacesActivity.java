4484558d593306c5c09adec90c4076fdbc16364e
Yuchen Jing <TheDuckyJason@gmail.com>
2020-04-20 02:44:44 -0700

    Feature: restyle the landmark list

--- app/src/main/java/com/example/navucsd/PlacesActivity.java
+++ app/src/main/java/com/example/navucsd/PlacesActivity.java
@@ -2,12 +2,14 @@ package com.example.navucsd;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
 
 import android.text.InputType;
 import android.util.TypedValue;
@@ -32,13 +34,35 @@ public final class PlacesActivity extends AppCompatActivity {
 	private boolean clicked;
 
 	/**
+	 * A landmark block, consisting of an image resource and a name.
+	 */
+	private static final class LandmarkInfo {
+		/**
+		 * The resource ID of the image of this landmark.
+		 */
+		private int resId;
+
+		/**
+		 * The name of this landmark.
+		 */
+		private String name;
+
+		/**
+		 * A convenient constructor.
+		 *
+		 * @param resId the resource id of the image
+		 * @param name the name of this landmark
+		 */
+		public LandmarkInfo(int resId, String name) {
+			this.resId = resId;
+			this.name = name;
+		}
+	}
+
+	/**
 	 * A smarter {@code ImageView} that automatically resizes its internal upon size change.
 	 */
 	private final class SmartImageView extends androidx.appcompat.widget.AppCompatImageView {
-		/**
-		 * The desired aspect ratio (width / height).
-		 */
-		private double aspectRatio;
 		/**
 		 * The last set width, defaults to 0 at first.
 		 * Used to detect changes and decide when to resize.
@@ -46,14 +70,17 @@ public final class PlacesActivity extends AppCompatActivity {
 		private int width;
 
 		/**
-		 * Constructs a {@code SmartImageView} with an image resource id and an aspect ratio.
+		 * Constructs a {@code SmartImageView} with an image resource id and an aspect ratio.  It
+		 * also resizes the parent to the height of this image view plus the height of the label.
+		 *
 		 * @param resId the resouce id of the image, if {@code null} the background is not set
 		 * @param aspectRatio the desired aspect ratio (width / height)
+		 * @param labelHeight the height of the label, in dp
 		 */
-		public SmartImageView(Integer resId, double aspectRatio) {
+		public SmartImageView(int resId, double aspectRatio, int labelHeight) {
 			super(PlacesActivity.this);
 
-			if (resId != null) setBackgroundResource(resId);
+			setBackgroundResource(resId);
 
 			this.getViewTreeObserver().addOnPreDrawListener(() -> {
 				int new_width = getWidth();
@@ -63,7 +90,13 @@ public final class PlacesActivity extends AppCompatActivity {
 
 					layout.height = (int) (new_width / aspectRatio);
 					width = new_width;
-					if (resId != null) setBackground(resize(resId, width, layout.height));
+					setBackground(resize(resId, width, layout.height));
+
+					View parent = (View) getParent();
+					ViewGroup.LayoutParams params = parent.getLayoutParams();
+					params.height = layout.height + dpToXp(labelHeight);
+					parent.setLayoutParams(params);
+
 					requestLayout();
 				}
 
@@ -114,30 +147,19 @@ public final class PlacesActivity extends AppCompatActivity {
 
 		TableLayout landmarkTableLayout = findViewById(R.id.landmarkTableLayout);
 
-		int res_ids[] = {
-				R.drawable.geisel,
-				R.drawable.peterson,
-				R.drawable.mayer,
-				R.drawable.price_center_east,
-				R.drawable.rady,
-				R.drawable.geisel,
-				R.drawable.peterson,
-				R.drawable.mayer,
-				R.drawable.price_center_east,
-		};
-		String names[] = {
-				"Geisel Library",
-				"Peterson Hall",
-				"Mayer Hall",
-				"Price Center East",
-				"Rady School of Mgt",
-				"Geisel Library",
-				"Peterson Hall",
-				"Mayer Hall",
-				"Price Center East",
+		LandmarkInfo landmarks[] = {
+			new LandmarkInfo(R.drawable.geisel, "Geisel Library"),
+			new LandmarkInfo(R.drawable.peterson, "Peterson Hall"),
+			new LandmarkInfo(R.drawable.mayer, "Mayer Hall"),
+			new LandmarkInfo(R.drawable.price_center_east, "Price Center East"),
+			new LandmarkInfo(R.drawable.rady, "Rady School of Mgt"),
+			new LandmarkInfo(R.drawable.geisel, "Geisel Library"),
+			new LandmarkInfo(R.drawable.peterson, "Peterson Hall"),
+			new LandmarkInfo(R.drawable.mayer, "Mayer Hall"),
+			new LandmarkInfo(R.drawable.price_center_east, "Price Center East"),
 		};
 
-		addLandmarks(res_ids, names);
+		addLandmarks(landmarks);
 	}
 
 	/**
@@ -151,96 +173,131 @@ public final class PlacesActivity extends AppCompatActivity {
 
 	/**
 	 * Adds the landmarks provided to the table.
-	 * If {@code resId} and {@code names} are {@code null}, nothing will be added.
-	 * @param resId the resource ids of the images
-	 * @param names the names of the landmarks
-	 * @throws IllegalArgumentException if {@code resId} and {@code names} are of different size or
-	 * one of them is null
-	 * @throws NullPointerException if {@code names} contains any {@code null} element
+	 *
+	 * @param landmarks the landmarks
+	 * @throws NullPointerException if {@code landmarks} is {@code null}, but the elements are not checked
 	 */
-	private void addLandmarks(int[] resId, String[] names) {
-		if (resId == null && names == null) return;
-		if (resId == null || names == null) {
-			throw new IllegalArgumentException("one of resId and names is null");
-		}
-		if (resId.length != names.length) {
-			throw new IllegalArgumentException("resId and names must have the same size");
-		}
-		if (resId.length == 0) return;
+	private void addLandmarks(LandmarkInfo[] landmarks) {
+		Objects.requireNonNull(landmarks, "landmark must not be null");
+		if (landmarks.length == 0) return;
+
+		final int BOTTOM_MARGIN_DP = 10, VERY_BOTTOM_MARGIN_DP = 5, GAP_DP = 10;
 
 		TableLayout landmarkTableLayout = findViewById(R.id.landmarkTableLayout);
-		boolean odd = resId.length % 2 != 0;
-		int length = odd ? resId.length - 1 : resId.length - 2;
+		boolean odd = landmarks.length % 2 != 0;
+		int length = odd ? landmarks.length - 1 : landmarks.length - 2;
 
 		for (int i = 0; i < length; i += 2) {
-			Objects.requireNonNull(names[i], "names contains a null element at index " + i);
-			landmarkTableLayout.addView(getImageRow(resId[i], resId[i + 1]));
-			landmarkTableLayout.addView(getLabelRow(true, names[i], names[i + 1]));
+			landmarkTableLayout.addView(getRow(BOTTOM_MARGIN_DP, GAP_DP, landmarks[i], landmarks[i + 1]));
 		}
 
 		if (odd) {
-			landmarkTableLayout.addView(getImageRow(resId[resId.length - 1]));
-			landmarkTableLayout.addView(getLabelRow(false, names[names.length - 1]));
+			landmarkTableLayout.addView(getRow(VERY_BOTTOM_MARGIN_DP, GAP_DP, landmarks[landmarks.length - 1]));
 		} else {
 			landmarkTableLayout.addView(
-				getImageRow(
-					resId[resId.length - 2],
-					resId[resId.length - 1]
+				getRow(
+					VERY_BOTTOM_MARGIN_DP,
+					GAP_DP,
+					landmarks[landmarks.length - 2],
+					landmarks[landmarks.length - 1]
 				)
 			);
 			landmarkTableLayout.addView(
-				getLabelRow(
-					false,
-					names[names.length - 2],
-					names[names.length - 1]
+				getRow(
+					VERY_BOTTOM_MARGIN_DP,
+					GAP_DP,
+					landmarks[landmarks.length - 2],
+					landmarks[landmarks.length - 1]
 				)
 			);
 		}
 	}
 
 	/**
-	 * Returns a new TableRow composed of 1 to 2 images.
+	 * Returns a new TableRow composed of 1 to 2 landmark blocks.
 	 * Behavior with a {@code null} or an incorrect number of {@code resId} is undefined.
-	 * @param resId the resource ids of the images, only 1 to 2 are supported
+	 * @param bottomMargin the bottom margin of this row, in dp
+	 * @param gap the space in between the landmark blocks, in dp
+	 * @param landmarks the landmarks, only 1 to 2 are supported
 	 * @return the new TableRow
+	 * @throws IllegalArgumentException when {@code gap} is not a multiple of 2 or
+	 * either {@code bottomMargin} or {@code gap} is negative
 	 */
-	private TableRow getImageRow(int... resId) {
+	private TableRow getRow(final int bottomMargin, final int gap, LandmarkInfo... landmarks) {
+		if (bottomMargin < 0 || gap < 0 || gap % 2 == 1) throw new IllegalArgumentException();
+
 		final double ASPECT_RATIO = 5.0 / 4;
-		final int HALF_MARGIN_DP = halfMargin();
+		final int LABEL_HEIGHT_DP = 30, SIDE_MARGIN_DP = 5, CORNER_RADIUS_DP = 5;
+		final int TEXT_SIZE_SP = 15;
 
 		TableRow row = new TableRow(this);
 
 		TableRow.LayoutParams layout_start, layout_end;
+		CardView.LayoutParams label_layout;
 
-		// set the start layout
 		layout_start = new TableRow.LayoutParams(
-				TableRow.LayoutParams.WRAP_CONTENT, // XXX 0?
-				0,
-				1
+			TableRow.LayoutParams.WRAP_CONTENT,
+			TableRow.LayoutParams.WRAP_CONTENT,
+			1
 		);
-		layout_start.setMarginEnd(dpToXp(HALF_MARGIN_DP));
+		layout_start.bottomMargin = dpToXp(bottomMargin);
+		layout_start.setMarginStart(dpToXp(SIDE_MARGIN_DP));
+		layout_start.setMarginEnd(dpToXp(gap / 2));
 
-		// set the end layout
 		layout_end = new TableRow.LayoutParams(
-				TableRow.LayoutParams.WRAP_CONTENT, // XXX 0?
-				0,
-				1
+			TableRow.LayoutParams.WRAP_CONTENT,
+			TableRow.LayoutParams.WRAP_CONTENT,
+			1
 		);
-		layout_end.setMarginStart(dpToXp(HALF_MARGIN_DP));
+		layout_end.bottomMargin = dpToXp(bottomMargin);
+		layout_end.setMarginStart(dpToXp(gap / 2));
+		layout_end.setMarginEnd(dpToXp(SIDE_MARGIN_DP));
 
-		SmartImageView image;
+		label_layout = new CardView.LayoutParams(
+			CardView.LayoutParams.MATCH_PARENT,
+			dpToXp(LABEL_HEIGHT_DP)
+		);
+		label_layout.gravity = Gravity.BOTTOM;
 
-		// add start column image
-		image = new SmartImageView(resId[0], ASPECT_RATIO);
-		image.setOnClickListener(getOnClickListener());
-		row.addView(image, layout_start);
+		CardView card;
+		TextView label;
 
-		// add end column image
-		image = new SmartImageView(resId.length == 2 ? resId[1] : null, ASPECT_RATIO);
-		image.setOnClickListener(getOnClickListener());
-		row.addView(image, layout_end);
+		card = new CardView(this);
+		card.setOnClickListener(getOnClickListener());
+		card.setRadius(dpToXp(CORNER_RADIUS_DP));
 
-		// set row layout
+		label = new TextView(this);
+		label.setBackgroundColor(0xFFFFFFFF);
+		label.setText(landmarks[0].name);
+		label.setTextColor(0xFF162B46);
+		label.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE_SP);
+		label.setTypeface(Typeface.DEFAULT_BOLD); // this avoids font weight and looks similar enough
+		label.setGravity(Gravity.CENTER);
+		label.setOnClickListener(getOnClickListener());
+
+		card.addView(new SmartImageView(landmarks[0].resId, ASPECT_RATIO, LABEL_HEIGHT_DP));
+		card.addView(label, label_layout);
+		row.addView(card, layout_start);
+
+		card = new CardView(this);
+		if (landmarks.length >= 2) {
+			card.setOnClickListener(getOnClickListener());
+			card.setRadius(dpToXp(CORNER_RADIUS_DP));
+
+			label = new TextView(this);
+			label.setBackgroundColor(0xFFFFFFFF);
+			label.setText(landmarks[1].name);
+			label.setTextColor(0xFF162B46);
+			label.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE_SP);
+			label.setTypeface(Typeface.DEFAULT_BOLD); // this avoids font weight and looks similar enough
+			label.setGravity(Gravity.CENTER);
+			label.setOnClickListener(getOnClickListener());
+
+			card.addView(new SmartImageView(landmarks[1].resId, ASPECT_RATIO, LABEL_HEIGHT_DP));
+			card.addView(label, label_layout);
+		}
+		row.addView(card, layout_end);
+
 		row.setLayoutParams(
 			new TableLayout.LayoutParams(
 				TableLayout.LayoutParams.MATCH_PARENT,
@@ -250,73 +307,6 @@ public final class PlacesActivity extends AppCompatActivity {
 		);
 
 		return row;
-	}
-
-	/**
-	 * Returns a new TableRow composed of 1 to 2 {@code TextView}s.
-	 * Behavior with a {@code null} or an invalid number of arguments is undefined.
-	 * @param bottomMargin to add bottom margin to separate rows or not
-	 * @param names the names of the images displayed on the labels
-	 * @return the new TableRow
-	 */
-	private TableRow getLabelRow(boolean bottomMargin, String... names) {
-		final int LABEL_HEIGHT_DP = 30;
-		final int TEXT_SIZE_SP = 14;
-		final int HALF_MARGIN_DP = halfMargin();
-
-		TableRow label_row = new TableRow(this);
-		TextView label;
-
-		TableRow.LayoutParams layout_start, layout_end;
-
-		// sets the start layout
-		layout_start = new TableRow.LayoutParams(
-				TableRow.LayoutParams.MATCH_PARENT,
-				dpToXp(LABEL_HEIGHT_DP),
-				1
-		);
-		layout_start.setMarginEnd(dpToXp(HALF_MARGIN_DP));
-
-		// sets the end layout
-		layout_end = new TableRow.LayoutParams(
-				TableRow.LayoutParams.MATCH_PARENT,
-				dpToXp(LABEL_HEIGHT_DP),
-				1
-		);
-		layout_end.setMarginStart(dpToXp(HALF_MARGIN_DP));
-
-		// add the first label
-		label = new TextView(this);
-		label.setBackgroundColor(0xFFEBECED);
-		label.setText(names[0]);
-		label.setTextColor(0xFF000000);
-		label.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE_SP);
-		label.setGravity(Gravity.CENTER);
-		label.setOnClickListener(getOnClickListener());
-		label_row.addView(label, layout_start);
-
-		// add the second label
-		label = new TextView(this);
-		if (names.length >= 2) {
-			label.setBackgroundColor(0xFFEBECED);
-			label.setText(names[1]);
-			label.setTextColor(0xFF000000);
-			label.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE_SP);
-			label.setGravity(Gravity.CENTER);
-			label.setOnClickListener(getOnClickListener());
-		}
-		label_row.addView(label, layout_end);
-
-		TableLayout.LayoutParams row_layout;
-		row_layout = new TableLayout.LayoutParams(
-			TableLayout.LayoutParams.MATCH_PARENT,
-			TableLayout.LayoutParams.WRAP_CONTENT
-		);
-		if (bottomMargin) row_layout.bottomMargin = dpToXp(halfMargin() * 2);
-
-		label_row.setLayoutParams(row_layout);
-
-		return label_row;
 	}
 
 	/**
@@ -344,13 +334,5 @@ public final class PlacesActivity extends AppCompatActivity {
 			dp,
 			getResources().getDisplayMetrics()
 		);
-	}
-
-	/**
-	 * Gets the constant for half of the margin.
-	 * @return the half margin constant
-	 */
-	private int halfMargin() {
-		return 10 / 2;
 	}
 }
