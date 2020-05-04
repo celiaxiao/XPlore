6f4f25d0bca4389f380a31d79cb7cec10d03c92a
蔡锐 <cairui@CR-MBP-13.lan>
2020-05-04 12:51:50 -0700

    Basic button and textView in TourOverviewPage

---
+++ app/src/main/java/com/example/navucsd/TourOverviewPage.java
@@ -0,0 +1,13 @@
+package com.example.navucsd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TourOverviewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_overview_page);
+    }
+}
