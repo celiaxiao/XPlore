bd0f2fdd59fee05659fd6a8c034da8de373dca64
Yuchen Jing <TheDuckyJason@gmail.com>
2020-05-04 21:36:37 -0700

    Refactor: rename app and packages to XploreUCSD

---
+++ app/src/main/java/org/ucsdtriplec/xploreucsd/ToursFragment.java
@@ -0,0 +1,49 @@
+package org.ucsdtriplec.xploreucsd;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ToursFragment extends Fragment {
+
+    ListViewForScrollView v;
+    ScrollView sv;
+
+    private String[] nameSet = {"UCSD's Landmark Tour", "Most Popular Restaurants at UCSD", "Intro to Brutalism - \nArchitecture of UCSD", "60 Years by the Beach - \nHistorical Sites at UCSD", "Let's Get Punky - \nHidden Artworks on Campus", "Revelle College"};
+    private int[] timeSet = {90, 30, 40, 50, 60, 70};
+    private int[] stopsSet = {5, 3, 2, 3, 4, 5};
+    private int[] pictures = {R.drawable.tours_landmark, R.drawable.tours_restaurants, R.drawable.tours_brutalism, R.drawable.tours_historical_sites, R.drawable.tours_hidden_artworks, R.drawable.tours_revelle};
+
+    public ToursFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tours, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        v = (ListViewForScrollView) getView().findViewById(R.id.tours_lv);
+        ToursAdapter a = new ToursAdapter(this.getActivity(), nameSet, timeSet, stopsSet, pictures);
+        v.setAdapter(a);
+
+        // Adjustment for ListViewForScrollView
+        sv = (ScrollView) getView().findViewById(R.id.tours_sv);
+        sv.smoothScrollTo(0, 0);
+    }
+}
