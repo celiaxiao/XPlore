958753d5313ffa19ef37bf5a84d713f6ba3167ea
Rui Cai <rucai@ucsd.edu>
2020-05-06 21:41:14 -0700

    Added CardView and Recycler

--- app/src/main/java/com/example/navucsd/TourOverviewPage.java
+++ app/src/main/java/com/example/navucsd/TourOverviewPage.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TourOverviewPage extends AppCompatActivity {
 
@@ -12,8 +17,13 @@ public class TourOverviewPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_overview_page);
 
-
-
+        FloatingActionButton fabButton = (FloatingActionButton) findViewById(R.id.floating_action_add_button_tour_overview_page);
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TourOverviewPage.this, "Testing", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
