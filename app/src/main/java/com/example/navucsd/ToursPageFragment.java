39855481966522348b0e1a8c3893bfbd6a3b7033
Yuchen Jing <TheDuckyJason@gmail.com>
2020-05-11 21:38:59 -0700

    Tours: link button (card) to guided tours

--- app/src/main/java/com/example/navucsd/ToursPageFragment.java
+++ app/src/main/java/com/example/navucsd/ToursPageFragment.java
@@ -1,8 +1,7 @@
 package com.example.navucsd;
 
-import android.graphics.Rect;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +10,6 @@ import android.widget.ScrollView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +24,11 @@ public class ToursPageFragment extends Fragment {
     private int[] stopsSet = {5, 3, 2, 3, 4, 5};
     private int[] pictures = {R.drawable.tours_landmark, R.drawable.tours_restaurants, R.drawable.tours_brutalism, R.drawable.tours_historical_sites, R.drawable.tours_hidden_artworks, R.drawable.tours_revelle};
 
+    /**
+     * If this page has been clicked, used to prevent multiple clicks.
+     */
+    private boolean clicked;
+
     public ToursPageFragment() {
         // Required empty public constructor
     }
@@ -37,7 +37,16 @@ public class ToursPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tours, container, false);
+        return inflater.inflate(R.layout.fragment_tours_page, container, false);
+    }
+
+    /**
+     * Called on resume of this fragment and resets the {@code clicked} attribute
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        clicked = false;
     }
 
     @Override
@@ -51,5 +60,24 @@ public class ToursPageFragment extends Fragment {
         // Adjustment for ListViewForScrollView
         sv = (ScrollView) getView().findViewById(R.id.tours_sv);
         sv.smoothScrollTo(0, 0);
+
+        getView()
+            .findViewById(R.id.cardViewCustomizeTour)
+            .setOnClickListener(getOnClickListener(TourOverviewPage.class));
+    }
+
+    /**
+     * Get a {@code OnClickListener} that starts a specified activity.
+     *
+     * @param target the activity to be started
+     * @return a {@code OnClickListener} that starts {@code target}
+     */
+    private View.OnClickListener getOnClickListener(Class<?> target) {
+        return view -> {
+            if (!clicked) {
+                clicked = true;
+                startActivity(new Intent(getActivity(), target));
+            }
+        };
     }
 }
