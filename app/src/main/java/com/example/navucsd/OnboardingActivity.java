facb302fc87155f79a22124060c73d3e093332d0
Rui Cai <rucai@ucsd.edu>
2020-05-14 18:38:25 -0700

    Dynamic dot indicator implemented

--- app/src/main/java/com/example/navucsd/OnboardingActivity.java
+++ app/src/main/java/com/example/navucsd/OnboardingActivity.java
@@ -1,26 +1,34 @@
 package com.example.navucsd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class OnboardingActivity extends AppCompatActivity {
-    private ViewPager mViewPagerOnboarding;
-    private LinearLayout mDotLayout;
-    private SliderAdapterOnboarding sliderAdapter;
-    private TextView[] mDots;
+    private ViewPager mViewPagerOnboarding; // Store Gifs
+    private LinearLayout mDotLayout; // linearLayout to display dot indicators
+    private SliderAdapterOnboarding sliderAdapter; // Adapter for viewPager
+    private ImageView[] mDots; // Dot indicators
+    private int dotsCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
+        SharedPreferences sharedPreferences = getPreferences(this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("Onboarding finished", true);
+        editor.commit();
         mViewPagerOnboarding = (ViewPager) findViewById(R.id.viewPagerOnboarding);
         mDotLayout = (LinearLayout) findViewById(R.id.linearLayoutOnboarding);
 
@@ -28,7 +36,42 @@ public class OnboardingActivity extends AppCompatActivity {
 
         mViewPagerOnboarding.setAdapter(sliderAdapter);
 
-        addDotsIndicator();
+        dotsCount = sliderAdapter.getCount();
+        mDots = new ImageView[dotsCount];
+
+        // Set all dots as inactive
+        for (int i = 0; i < dotsCount; i++) {
+            mDots[i] = new ImageView(this);
+            mDots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.dots_indicator_inactive));
+
+            // Set params for the linearLayout
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+                            (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(19, 0, 19, 0);
+            mDotLayout.addView(mDots[i], params);
+        }
+
+        // Set the first dot as the active dot
+        mDots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.dots_indicator_active));
+
+        mViewPagerOnboarding.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
+
+            @Override
+            public void onPageSelected(int position) {
+                // Set all dots as inactive
+                for (int i = 0; i < dotsCount; i++) {
+                    mDots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.dots_indicator_inactive));
+                }
+
+                // Set the corresponding dot as active
+                mDots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.dots_indicator_active));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) { }
+        });
 
 
     }
@@ -37,23 +80,6 @@ public class OnboardingActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        SharedPreferences sharedPreferences = getPreferences(this.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean("Onboarding finished", true);
-        editor.commit();
-
     }
 
-    public void addDotsIndicator() {
-        mDots = new TextView[3];
-
-        for (int i = 0; i < mDots.length; i++) {
-            mDots[i] = new TextView(this);
-            mDots[i].setText(Html.fromHtml("&#8226"));
-            mDots[i].setTextSize(35);
-            mDots[i].setTextColor(Color.parseColor("#006A96"));
-
-            mDotLayout.addView(mDots[i]);
-        }
-    }
 }
