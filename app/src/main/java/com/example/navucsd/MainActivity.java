dad2cd24f02700824c4abbcc3d73afe90d4c16e6
Xinyue Yu <xiy210@ucsd.edu>
2020-05-25 21:14:30 -0700

    Add file: GpsUtil.java to get location and modify MainActivity.java to ask for location permission

--- app/src/main/java/com/example/navucsd/MainActivity.java
+++ app/src/main/java/com/example/navucsd/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.navucsd;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -20,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
     private final String[] TAB_TITLES = {"Places", "", "Tours"};
     private final int MAIN_PAGE_INDEX = 1;
     private ViewPager mainPager;
+    private Location location;
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1000;
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+    private String disabledLocationInApp = "disabledLocationInApp";
+    private boolean isDisabled;
+    private double longitude;
+    private double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +61,61 @@ public class MainActivity extends AppCompatActivity {
         TabLayout.Tab mainPageTab = tabLayout.getTabAt(MAIN_PAGE_INDEX);
         mainPageTab.setCustomView(R.layout.custom_main_button);
         tabLayout.selectTab(mainPageTab);
+
+
+        // Check if user has explicitly disabled location permission in app
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        isDisabled = sharedPref.getBoolean(disabledLocationInApp, false);
+
+        // Check if location permission is granted
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission( this,
+                Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Log.d("location", "Permission not yet granted");
+            if (!isDisabled) {
+                // Has not explicitly disabled location in app
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_FINE_LOCATION);
+            }
+        }
+        else {
+            // Permission has already been granted
+            Log.d("location", "Permission already granted");
+            location = GpsUtil.getInstance(MainActivity.this).getLastLocation();
+            if (location != null) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    Log.d("location", "Call back: Permission granted");
+                    location = GpsUtil.getInstance(MainActivity.this).getLastLocation();
+                    if (location != null) {
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                    }
+                    editor.putBoolean(disabledLocationInApp, false);
+                    editor.apply();
+                }
+                else {
+                    // permission denied
+                    Log.d("location", "Call back: Permission denied");
+                    editor.putBoolean(disabledLocationInApp, true);
+                    editor.apply();
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     private class MainPagerAdapter extends FragmentStatePagerAdapter {
