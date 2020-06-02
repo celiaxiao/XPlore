f80f971a58309d282d8b2ed53906af0fcf45b410
Rui Cai <rucai@ucsd.edu>
2020-06-01 19:12:09 -0700

    Fix the problem of Onboarding Page not opening

--- app/src/main/java/com/example/navucsd/SplashPageActivity.java
+++ app/src/main/java/com/example/navucsd/SplashPageActivity.java
@@ -16,6 +16,7 @@ import java.util.TimerTask;
 
 public class SplashPageActivity extends AppCompatActivity {
     public static final int SPLASH_TIME_OUT = 1500;
+    public static final String APP_FIRST_RUN = "App first run";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +36,12 @@ public class SplashPageActivity extends AppCompatActivity {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-
-                // TODO: Debug? SharedPreference does not work as expected,
-                //  onboarding page doesn't start
                 SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-                if (sharedPreferences.getBoolean("Onboarding finished", false)) {
+                if (sharedPreferences.getBoolean(APP_FIRST_RUN, true)) {
                     Intent intent = new Intent(getApplicationContext(), OnboardingActivity.class);
                     startActivity(intent);
                     finish(); // Close onboarding
+                    sharedPreferences.edit().putBoolean(APP_FIRST_RUN, false).commit(); // Tell the app it has already finished its first run
                 } else {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     intent.putExtra("Splash", true);
